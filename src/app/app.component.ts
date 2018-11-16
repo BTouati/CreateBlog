aa034673ec9275b12451873dd456f1075e367e08
BTouati <benjamintouati.bt@gmail.com>
2018-11-16 11:40:31 +0100

    Color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CreateBlog';
+  post1=
+  { title: 'post1',
+    content: "Longtemps, je me suis couché de bonne heure. Parfois, à peine ma bougie éteinte, mes yeux se fermaient si vite que je n’avais pas le temps de me dire : « Je m’endors. » Et, une demi-heure après, la pensée qu’il était temps de chercher le sommeil m’éveillait ;",
+    loveIts: 0,
+    created_at: new Date()
+
+  };
+
+Posts=
+[
+  { title: 'post1',
+    content: "Longtemps, je me suis couché de bonne heure. Parfois, à peine ma bougie éteinte, mes yeux se fermaient si vite que je n’avais pas le temps de me dire : « Je m’endors. » Et, une demi-heure après, la pensée qu’il était temps de chercher le sommeil m’éveillait ;",
+    loveIts: 0,
+    created_at: new Date()
+
+  },
+  { title: 'post2',
+    content: "Longtemps, je me suis couché de bonne heure. Parfois, à peine ma bougie éteinte, mes yeux se fermaient si vite que je n’avais pas le temps de me dire : « Je m’endors. » Et, une demi-heure après, la pensée qu’il était temps de chercher le sommeil m’éveillait ;",
+    loveIts: 0,
+    created_at: new Date()
+
+  },
+  { title: 'post3',
+    content: "Longtemps, je me suis couché de bonne heure. Parfois, à peine ma bougie éteinte, mes yeux se fermaient si vite que je n’avais pas le temps de me dire : « Je m’endors. » Et, une demi-heure après, la pensée qu’il était temps de chercher le sommeil m’éveillait ;",
+    loveIts: 0,
+    created_at: new Date()
+
+  },
+  { title: 'post4',
+    content: "Longtemps, je me suis couché de bonne heure. Parfois, à peine ma bougie éteinte, mes yeux se fermaient si vite que je n’avais pas le temps de me dire : « Je m’endors. » Et, une demi-heure après, la pensée qu’il était temps de chercher le sommeil m’éveillait ;",
+    loveIts: 0,
+    created_at: new Date()
+
+  }
+
+]
 }
---
+++ src/app/post-list-item/post-list-item.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list-item',
+  templateUrl: './post-list-item.component.html',
+  styleUrls: ['./post-list-item.component.sass']
+})
+export class PostListItemComponent implements OnInit {
+  @Input() title: string;
+  @Input() content: string;
+  @Input() loveIts: number;
+  @Input() created_at: Date
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  LovePlus() {
+    this.loveIts++;
+    return this.loveIts;
+    }
+  LoveMinus() {
+    this.loveIts--;
+    return this.loveIts;
+    }
+
+}
